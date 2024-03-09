da06057097977fefac5ade976d2ba1b2f8534793
DARIA-MARIA MATEI <daria.maria.matei@stud.ubbcluj.ro>
2024-03-09 12:16:32 +0200

    Initial commit

---
+++ src/main/java/com/example/manageaid/ManageAidApplication.java
@@ -0,0 +1,13 @@
+package com.example.manageaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageAidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageAidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/manageaid/ManageAidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.manageaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageAidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
