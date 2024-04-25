8fa83371d903f33439a2fc808f1654f541da91f6
DARIA-MARIA MATEI <daria.maria.matei@stud.ubbcluj.ro>
2024-04-25 18:06:13 +0300

    JWT basic

--- src/main/java/com/example/manageaid/ManageAidApplication.java
+++ src/main/java/com/example/manageaid/ManageAidApplication.java
@@ -1,8 +1,11 @@
 package com.example.manageaid;
 
+import com.example.manageaid.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class ManageAidApplication {
 
